cf0bd01b3067cb36544bf02ba486d6e05615dcbc
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-03-25 14:41:19 -0700

    Add logic to get Nasa photo to model

--- app/models/nasa_api.rb
+++ app/models/nasa_api.rb
@@ -1,2 +1,9 @@
 class NasaApi < ApplicationRecord
+  def self.get_api_info
+    nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
+    request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
+    JSON.parse request_to_nasa_api
+
+  ))
+  end
 end
